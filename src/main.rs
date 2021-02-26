df7b88bf6048a113b000eab3330a58e121895e55
YukiUmetsu <yuuki.umetsu@gmail.com>
2021-02-25 22:04:32 -0600

    simple snake game can be played

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rand;
 
 mod draw;
 mod game;
+mod snake;
 
 use crate::draw::to_coord_u32;
 use crate::game::Game;
@@ -22,6 +23,10 @@ fn main() {
 
     let mut game = Game::new(width, height);
     while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        };
+
         window.draw_2d(&event, |c, g, _| {
             clear(BACK_COLOR, g);
             game.draw(&c, g);
